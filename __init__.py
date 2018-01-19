6eec9bb6f7cfd71c586faccd6e1aacf7e3725608
Jean Da Costa Machado <blendeto@gmail.com>
2018-01-19 19:06:53 -0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ Jean3dimensional@gmail.com
 
 Created by Jean Da Costa Machado
 
-    This program is free software: you can redistribute it and/or modify
+    This program and any python file in this directory is free software,
+    you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
